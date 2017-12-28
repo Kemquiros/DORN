4dcfc7f9a4e4dee212afe5ccb24263898a0486af
kemquiros <absurdoeco@gmail.com>
2017-12-28 01:02:06 -0500

    Guelin change to Avatar model

--- src/com/dorn/model/heroe/Guelin.java
+++ src/com/dorn/model/heroe/Guelin.java
@@ -14,8 +14,9 @@ import java.util.ArrayList;
  */
 public class Guelin extends Heroe{
     public Guelin(){
-        this.nombre="Guelin";
-        this.ataqueFisico=true;
+        setNombre("Guelin");
+        isAtaqueFisico(true);        
+
         this.habilidad = new ArrayList();
         this.setRutaImagen("/com/dorn/assets/heroe/img/guelin.png");       
         this.setRutaCarta("/com/dorn/assets/heroe/card/guelin.png");
@@ -27,20 +28,20 @@ public class Guelin extends Heroe{
     public void subirNivelUno() {
         setNivel(1);
         
-        this.movimientoMax=6;
+        setMovimientoMax(6);
         
-        this.vidaMax=5;
-        this.vida=vidaMax;
+        setVidaMax(5);
+        setVida(getVidaMax());
         
-        this.ataqueMax=1;
+        setAtaqueMax(1);
         //0:Mágico
         //1:Contacto
         //2:Pica
         //3:Arco        
         setTipoAtaque(3);        
         
-        this.experienciaMax=5;
-        this.experiencia=0;
+        setExperienciaMax(5);
+        setExperiencia(0);
         //Habilidades
         //--------------------------------------
         Habilidad h = new Habilidad();
